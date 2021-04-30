b82127a847de462e16b73aaffa02b6ad804d5344
hamzsait <hamzasait@utexas.edu>
2021-04-30 11:01:59 -0500

    changed route of db for server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
+const sequelize = require('./dbConnection/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
