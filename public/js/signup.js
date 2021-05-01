4a7132954c13b056e27f7af3c7585c0dd7da8f84
hamzsait <hamzasait@utexas.edu>
2021-05-01 03:47:26 -0500

    added signup.js file for siging up an account

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+document.querySelector("#signupButton").addEventListener("click", async () => {
+    const username = (document.querySelector("#floatingInput").value)
+    const password = (document.querySelector("#floatingPassword").value)
+    const confirmPassword = (document.querySelector("#confirmPassword").value)
+    console.log(username)
+    console.log(password)
+    console.log(confirmPassword)
+    if(username && password && confirmPassword){
+
+       const response =  await fetch('/api/users/signup',{
+            method: 'POST',
+            body: JSON.stringify({username,password,confirmPassword}),
+            headers: { 'Content-Type': 'application/json' },
+       })
+
+       if (response.ok) {
+        document.location.replace('/');
+       } else {
+        alert('Failed to sign up.');
+       }
+
+    }
+
+})
