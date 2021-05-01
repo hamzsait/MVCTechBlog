f3142eaf476a4ca67df6d38f3960cbafbf858529
hamzsait <hamzasait@utexas.edu>
2021-04-30 22:11:04 -0500

    commented out functions not in use yet

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+//const helpers = require('./utils/helpers');
 const sequelize = require('./dbConnection/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ 
+  //helpers 
+});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -17,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use('/',routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
