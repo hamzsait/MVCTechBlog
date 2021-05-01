b76dc12265972a6ea612ccc9dadbe40cdbd0dd9d
hamzsait <hamzasait@utexas.edu>
2021-05-01 01:39:37 -0500

    added extra routes

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -2,11 +2,29 @@ const router = require('express').Router();
 // add database connection here later
 // add authention function in utils later
 
-router.get("/", async (req, res) => {
+router.get("/home", async (req, res) => {
     try{
         res.render('homepage')
     }
     catch (err){
+        res.json(err)
+    }
+})
+
+router.get("/login", async (req,res) => {
+    try{
+        res.render('login')
+    }
+    catch(err){
+        res.json(err)
+    }
+})
+
+router.get("dashboard", async (res,res) => {
+    try{
+        res.render("dashboard")
+    }
+    catch(err){
         res.json(err)
     }
 })
