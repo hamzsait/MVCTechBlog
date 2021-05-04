a90547c87dbf10d6ce88d139810fd55782467e7d
hamzsait <hamzasait@utexas.edu>
2021-05-03 21:49:17 -0500

    added delete and save button event listeners

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,3 +1,26 @@
-document.querySelector("#deleteButton").addEventListener("click", function(){
-    console.log(document.querySelector("#projectId").innerHTML.trim())
+document.querySelector("#deleteButton").addEventListener("click", async function(){
+    id = document.querySelector("#projectId").innerHTML.trim()
+    console.log(id)
+    await fetch(`/api/projects/${id}`,{
+        method: 'DELETE',
+    }).then(response => window.location.href = "/")
+})
+
+document.querySelector("#saveButton").addEventListener("click", async function(){
+    id = document.querySelector("#projectId").innerHTML.trim()
+    projectName = document.querySelector("#projectName").value
+    projectDescription = document.querySelector("#projectDescription").innerHTML
+    user = document.querySelector("#username").innerHTML.trim()
+
+    output = {
+        id:id,
+        name:projectName,
+        description:projectDescription,
+        user:user
+    }
+    console.log(output)
+    await fetch(`api/projects/${id}`,{
+        method:'PUT',
+        body:output
+    })
 })
