4db854ae808b73e6c13e39c8a2d64ad6daed85dd
hamzsait <hamzasait@utexas.edu>
2021-05-03 21:48:59 -0500

    put route has been added but has some flaws

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -3,9 +3,20 @@ const { Project } = require('../../models');
 
 
 router.delete('/:id', async (req, res) => {
-
-    const projectData = await Project.destroy({where: {id:req.params.id}})
+    await Project.destroy({where: {id:req.params.id}})
     res.status(200).redirect('/')
+})
+
+router.put('/:id', async (req, res) => {
+    if (req.body.id == req.params.id){
+        Project.update(
+            {where:{id:req.body.id}},
+            {
+                name:req.body.name,
+                description:req.body.name,
+            }
+        )
+    }
 })
 
 module.exports = router
