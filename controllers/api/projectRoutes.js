906c82a4f2fa22a98346f71a677b478971a7c2e5
hamzsait <hamzasait@utexas.edu>
2021-05-04 00:07:27 -0500

    updated put and post routes

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,22 +1,29 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Project, User } = require('../../models');
 
 
 router.delete('/:id', async (req, res) => {
+    console.log("accessed")
     await Project.destroy({where: {id:req.params.id}})
     res.status(200).redirect('/')
 })
 
 router.put('/:id', async (req, res) => {
-    if (req.body.id == req.params.id){
-        Project.update(
-            {where:{id:req.body.id}},
-            {
-                name:req.body.name,
-                description:req.body.name,
-            }
-        )
-    }
+    await Project.update(
+        {name:req.body.name, description:req.body.description},
+        {where: {id: req.body.id}}
+    )
+})
+
+router.post('/', async (req, res) => {
+    await User.findOne({where:{username:req.body.user}}).then((response) =>{
+        output = {
+            name:req.body.name,
+            description:req.body.description,
+            user_id:response.dataValues.id
+        }
+        Project.create(output)
+    })
 })
 
 module.exports = router
